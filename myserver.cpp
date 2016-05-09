811a4c4eec2b9718253e7444be2ae6597b491f96
agostinhobritojr <agostinhobritojr@gmail.com>
2016-05-09 06:53:24 -0300

    server

---
+++ myserver.cpp
@@ -0,0 +1,24 @@
+#include "myserver.h"
+
+MyServer::MyServer(QObject *parent) :
+  QTcpServer(parent)
+{
+  ok = 1;
+}
+
+void MyServer::StartServer(){
+  if(!this->listen(QHostAddress::Any, 1234)){
+    qDebug() << "server not started";
+  }
+  else{
+    qDebug() << "server started";
+  }
+}
+
+void MyServer::incomingConnection(qintptr socketDescriptor){
+
+  qDebug() << socketDescriptor << " connecting...";
+  MyThread *thread = new MyThread(socketDescriptor,this, &storage);
+  connect(thread,SIGNAL(finished()), thread, SLOT(deleteLater()));
+  thread->run();
+}
